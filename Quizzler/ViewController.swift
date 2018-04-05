53ed662ce2488572191d152fc81f9895c05785a5
Fer Bueno <ferbuenob@gmail.com>
2018-04-04 19:35:22 -0500

    When tapping a button the question now changes & it prints correct and incorrect. When finishing the test, the test is restarted

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionIndex : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,31 +23,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
     
     func updateUI() {
-      
+      questionLabel.text = allQuestions.list[questionIndex].questionText
     }
     
 
     func nextQuestion() {
-        
+        if questionIndex < (allQuestions.list.count - 1) {
+            questionIndex += 1
+            updateUI()
+        } else {
+            startOver()
+        }
     }
     
     
     func checkAnswer() {
+        let currentQuestionAnswer = allQuestions.list[questionIndex].answer
+        if currentQuestionAnswer == pickedAnswer {
+            print("Correct")
+        } else {
+            print("Incorrect")
+        }
+        nextQuestion()
         
     }
     
     
     func startOver() {
-       
+       questionIndex = 0
+        updateUI()
     }
     
 
